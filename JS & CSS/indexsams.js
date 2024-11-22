ab268009539898e893e10c3f626e1b2a27adfb44
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-11-22 14:53:36 +0100

    Update 0.5.31 - Closed opositions for the SAMS

--- JS & CSS/indexsams.js
+++ JS & CSS/indexsams.js
@@ -51,7 +51,7 @@ document.getElementById('theme-toggle').addEventListener('click', function () {
 
 // Mostrar/ocultar sección de formulario al hacer clic en el botón "Opositar"
 document.getElementById('opositar-button').addEventListener('click', function (event) {
-  const oposicionesAbiertas = true; 
+  const oposicionesAbiertas = false; 
   const opositarLink = document.getElementById('opositar-link');
 
   if (!oposicionesAbiertas) {
